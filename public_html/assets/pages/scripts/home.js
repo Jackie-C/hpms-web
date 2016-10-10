9154f8ff491baf79966b66e0b01fa870b4699209
Jackie-C <Jackie.Cheung@student.uts.edu.au>
2016-10-10 23:56:29 +1100

    Bugfix for home.js

--- public_html/assets/pages/scripts/home.js
+++ public_html/assets/pages/scripts/home.js
@@ -1,7 +1,7 @@
 /*
 var jsonObject = {"took":24,"timed_out":false,"_shards":{"total":35,"successful":35,"failed":0},"hits":{"total":17046,"max_score":0.0,"hits":[]},"aggregations":{"1":{"value":7209.464507283031},"2":{"value":7209.464507283031},"3":{"value":22.256084112057735},"4":{"value":49.953888806052724},"5":{"value":7209.464507283031},"6":{"value":7209.464507283031},"7":{"value":0.10573542923974878},"8":{"value":2},"9":{"value":1802.3661268207577}}};
 */
-$(document).ready(function() {
+$(function() {
 	var jsonObject = null;
 	
 	$.ajax({
@@ -20,25 +20,24 @@ $(document).ready(function() {
 		success: function(data) {
 			jsonObject = data;
 			checkResponse();
+			// Tier 1 (Stats above chart)
+			$("#currentEnergyUsage").attr('data-value', Math.round(jsonObject.aggregations["1"].value));
+			$("#totalDailyEnergyUsage").attr('data-value', Math.round(jsonObject.aggregations["2"].value));
+			$("#temperature").attr('data-value', Math.round(jsonObject.aggregations["3"].value));
+			$("#humidity").attr('data-value', Math.round(jsonObject.aggregations["4"].value));
+
+			// Tier 2 (FlotChart)
+			// TODO
+
+			// Tier 3 (General Stats)
+			$("#numberTransactions").text((Math.round(jsonObject.aggregations["5"].value) / Math.round(jsonObject.aggregations["6"].value) * 100) + " %");
+			$("#dailyRunningTotal").text("$ " + (Math.round(jsonObject.aggregations["7"].value)).toFixed(2));
+			$("#largestConsumption").text("Television");
+			$("#monthlyRunningTotal").text("$ " + (Math.round(jsonObject.aggregations["9"].value)).toFixed(2));
 		}
 	});
 		
 	function checkResponse() {
 		console.log(jsonObject);
 	}
-
-    // Tier 1 (Stats above chart)
-    $("#currentEnergyUsage").attr('data-value', Math.round(jsonObject.aggregations["1"].value));
-    $("#totalDailyEnergyUsage").attr('data-value', Math.round(jsonObject.aggregations["2"].value));
-    $("#temperature").attr('data-value', Math.round(jsonObject.aggregations["3"].value));
-    $("#humidity").attr('data-value', Math.round(jsonObject.aggregations["4"].value));
-
-    // Tier 2 (FlotChart)
-    // TODO
-
-    // Tier 3 (General Stats)
-    $("#numberTransactions").text((Math.round(jsonObject.aggregations["5"].value) / Math.round(jsonObject.aggregations["6"].value) * 100) + " %");
-    $("#dailyRunningTotal").text("$ " + (Math.round(jsonObject.aggregations["7"].value)).toFixed(2));
-    $("#largestConsumption").text("Television");
-    $("#monthlyRunningTotal").text("$ " + (Math.round(jsonObject.aggregations["9"].value)).toFixed(2));
 });
